a275e6318d9d3b9bc82942a69605942074181547
Hrithik Bhardwaj <hrithikbhardwaj@Hrithiks-MacBook-Air.local>
2026-04-01 14:07:19 +0530

    fix syntax

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-    console.log(\`Server running at http://0.0.0.0:${PORT}`);
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
     
    
